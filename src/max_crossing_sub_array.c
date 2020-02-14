5e23968781086d206abe0c17d35cd3b103993152
r0bin-dood <r0bin_dood@pm.me>
2020-02-13 23:41:38 -0500

    minor addition to max crossing

--- src/max_crossing_sub_array.c
+++ src/max_crossing_sub_array.c
@@ -2,6 +2,9 @@
 
 typedef struct {
     union {
+        int low;
+        int mid;
+        int high;
         int max_left;
         int max_right;
         int left_low;
@@ -14,6 +17,7 @@ typedef struct {
         int cross_high;
         int cross_sum;
         int sum;
+        int temp;
     } * val;
 } Triple;
 
@@ -21,9 +25,10 @@ struct *
 find_max_crossing_subarray(int * A, const int low, const int mid, const int high)
 {
     Triple * ret = malloc(sizeof(Triple));
-    ret->val = malloc(sizeof(int) * 3);
+    ret->val = malloc(5 * sizeof(int));
     
     int i;
+    
     ret->val[0].left_sum = INT_MIN;
     ret->val[1].right_sum = INT_MIN;
 
@@ -32,10 +37,10 @@ find_max_crossing_subarray(int * A, const int low, const int mid, const int high
     for (i = mid; i > low; --i)
     {
         ret->val[2].sum = ret->val[2].sum + *(A + i);
-        if (ret->val[2].sum > left_sum)
+        if (ret->val[2].sum > ret->val[0].left_sum)
         {
-            left_sum = ret->val[2].sum;
-            ret->max_left = i;
+            ret->val[0].left_sum = ret->val[2].sum;
+            ret->val[3].max_left = i;
         }
     }
 
@@ -43,15 +48,33 @@ find_max_crossing_subarray(int * A, const int low, const int mid, const int high
     for (i = mid + 1; i < high; ++i)
     {
         ret->val[2].sum = ret->val[2].sum + *(A + i);
-        if (ret->val[2].sum > right_sum)
+        if (ret->val[2].sum > ret->val[1].right_sum)
         {
-            right_sum = ret->val[2].sum;
-            ret->max_right = i;
+            ret->val[1].right_sum = ret->val[2].sum;
+            ret->val[4].max_right = i;
         }
     }
 
-    ret->sum = left_sum + right_sum;
+    ret->val[2].sum = ret->val[0].left_sum + ret->val[1].right_sum;
 
     return ret;
 }
 
+struct *
+find_maximum_array(int * A, const int low, const int high)
+{
+    Triple * ret = malloc(sizeof(Triple));
+    ret->val = malloc();
+
+    if (high == low)
+    {
+        ret->val[].low = low;
+        ret->val[].high = high;
+        ret->val[].temp = *(A + low);
+        return ret;
+    } else {
+        //TODO
+
+    }
+        
+}
